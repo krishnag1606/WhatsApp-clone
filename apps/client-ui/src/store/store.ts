d87b2177018b3d0c678d6068f38f6f2d7fae296c
Krishna <143890852+krishnag1606@users.noreply.github.com>
2025-06-29 12:53:57 +0530

    Update store.ts

--- apps/client-ui/src/store/store.ts
+++ apps/client-ui/src/store/store.ts
@@ -2,14 +2,6 @@ import { create } from "zustand";
 import { ICredentials, IStore, IConversation } from "./IStore";
 
 export const useStore = create<IStore>((set) => ({
-  // credentials: {
-  //   name: "",
-  //   given_name: "Shrikanth",
-  //   email: "shrikanthsp22@gmail.com",
-  //   sub: "100862940514861011415",
-  //   picture:
-  //     "https://lh3.googleusercontent.com/a/ACg8ocI3utg5ppH18592-WF3IYkBATpwsobcQw_roovCE36tIQM7QIg=s96-c",
-  // } as ICredentials,
   credentials: null,
   setCredentials: (newCredentials: any) =>
     set((state: IStore) => ({ credentials: newCredentials })),
